ee554f43637f7977daf87f68b1f89c5c1b2105d1
shiroha <keqing.hu@icloud.com>
2022-03-02 15:04:55 +0800

    feat: 增加了部分 redis 的工具类方法

---
+++ portable-manager/src/main/java/com/portable/server/kit/RedisHashKit.java
@@ -0,0 +1,34 @@
+package com.portable.server.kit;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author shiroha
+ */
+public interface RedisHashKit {
+
+    /**
+     * 创建 redis hash
+     * @param prefix 前缀
+     * @param key key
+     * @param map data
+     */
+    void create(String prefix, Object key, Map<Long, Integer> map);
+
+    /**
+     * 获取值
+     * @param prefix 前缀
+     * @param key key
+     * @param index 索引
+     * @return 值
+     */
+    Optional<Integer> get(String prefix, Object key, Long index);
+
+    /**
+     * 清空 hash
+     * @param prefix 前缀
+     * @param key key
+     */
+    void clear(String prefix, Object key);
+}
