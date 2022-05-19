be192909968346519dbf53c5508816039859f4b1
shiroha <keqing.hu@icloud.com>
2022-05-19 20:25:48 +0800

    refactor: 使用 AOP 约束权限

---
+++ portable-start/src/main/java/com/portable/server/aop/PermissionMethodAspect.java
@@ -0,0 +1,34 @@
+package com.portable.server.aop;
+
+import com.portable.server.annotation.PermissionRequirement;
+import com.portable.server.exception.PortableException;
+import com.portable.server.type.PermissionType;
+import com.portable.server.util.UserContext;
+import org.apache.logging.log4j.util.Strings;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @author shiroha
+ */
+@Aspect
+@Component
+public class PermissionMethodAspect {
+
+    @Around("@annotation(com.portable.server.annotation.PermissionRequirement) && @annotation(permissionRequirement)")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint, PermissionRequirement permissionRequirement) throws Throwable {
+        String needPermission = Arrays.stream(permissionRequirement.value())
+                .filter(permissionType -> !UserContext.ctx().getPermissionTypeSet().contains(permissionType))
+                .map(PermissionType::getText)
+                .collect(Collectors.joining(", "));
+        if (!Strings.isBlank(needPermission)) {
+            throw PortableException.of("A-02-002", needPermission);
+        }
+        return proceedingJoinPoint.proceed();
+    }
+}
